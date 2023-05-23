dbb752c457d936a636ae5ba5a4702fff4742aa46
EduMLAlmeida <edualmeidawebdev@gmail.com>
2023-05-23 10:51:17 -0300

    Atende requisito 03.

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -1,4 +1,29 @@
 # Req 3
+from models.dish import Dish
+from models.ingredient import Ingredient
+import pandas as pd
+
+
 class MenuData:
     def __init__(self, source_path: str) -> None:
-        pass
+        self.dishes = set()
+
+        dish_prices = dict()
+
+        df = pd.read_csv(source_path)
+
+        price_data = df[['dish', 'price']].itertuples(index=False)
+
+        for dish, price in price_data:
+            dish_prices[dish] = price
+
+        for dish in dish_prices.keys():
+            dish_object = Dish(dish, dish_prices[dish])
+            ingredient_data = df.loc[df['dish'] == dish].itertuples(
+                index=False)
+            for line_dish, price, ingredient, amount in ingredient_data:
+                ingredient_object = Ingredient(ingredient)
+                dish_object.add_ingredient_dependency(
+                    ingredient_object, amount
+                )
+            self.dishes.add(dish_object)
